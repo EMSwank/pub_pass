13876dbe306b4a415f2873d981c7182860ad0493
Eliot Swank <eliotswank@gmail.com>
2018-05-22 14:01:03 -0600

    completes log out test GREEN

--- spec/views/users/user_can_log_in_spec.rb
+++ spec/views/users/user_can_log_in_spec.rb
@@ -47,7 +47,21 @@ describe "Vistor" do
 
       user = User.create(username: username, password: password)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/'
+
+      click_on "Log in"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :username,	with: username
+      fill_in :password,	with: password
+      click_on "Let me in"
+
+      click_on "Log Out"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_content("Log Out")
+      expect(page).to have_content("Log in")
     end
   end
 end
