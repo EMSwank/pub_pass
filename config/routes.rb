61156b526377909f20a3de20acfba2e1634ecafd
Eliot Swank <eliotswank@gmail.com>
2018-05-22 14:59:21 -0600

    adds admin namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  
+
   resources :breweries
   resources :beers
   resources :users
+  namespace :admin do
+    resources :categories, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
