456c5bd4082de9ea12f449e3ffb3e3eacb147450
Eliot Swank <eliotswank@gmail.com>
2018-05-22 08:58:36 -0600

    adds validations and secure_password to users

--- spec/views/users/user_can_log_in_spec.rb
+++ spec/views/users/user_can_log_in_spec.rb
@@ -14,13 +14,13 @@ describe "Vistor" do
 
       expect(current_path).to eq(login_path)
 
-    fill_in :username,	with: username
-    fill_in :password,	with: password
-    click_on "Log in"
+      fill_in :username,	with: username
+      fill_in :password,	with: password
+      click_on "Log in"
 
-    expect(current_path).to eq(user_path(user))
-    expect(page).to have_link("Log in")
-    end
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_link("Log in")
+      end
 
     
   end
