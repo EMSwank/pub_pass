ef8eb9fcf98c90967b81a734a9b5fc80aed2e115
Eliot Swank <eliotswank@gmail.com>
2018-05-22 11:33:07 -0600

    adds user and welcome controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  root 'welcome#index'
+  get 'welcome/index'
+
   resources :breweries
   resources :beers
+  resources :users, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/views/users/user_can_log_in_spec.rb
+++ spec/views/users/user_can_log_in_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 describe "Vistor" do
+  context "signs up to be a user." do
+    it "Completes the sign-up process" do
+    visit '/'
+
+    click_on 'Sign Up!'
+
+    expect(current_path).to eq(new_user_path)
+    
+    fill_in "user[username]",	with: "Guzzler"
+    fill_in "user[password]",	with: "password"
+
+    click_on 'Create User'
+
+    expect(page).to have_content("Welcome, Guzzler!") 
+    end
+  end
+  
+
   context "logs in." do
-    it "should log in the user" do
+    xit "should log in the user" do
       username = 'Humphrey'
       password = 'Bogart'
 
@@ -21,7 +39,5 @@ describe "Vistor" do
       expect(current_path).to eq(user_path(user))
       expect(page).to have_link("Log in")
       end
-
-    
   end
 end
