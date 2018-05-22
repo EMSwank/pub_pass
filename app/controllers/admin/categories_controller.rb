6a7bd4bdf25f37057e3ec48c6dc98422e1b3a932
Eliot Swank <eliotswank@gmail.com>
2018-05-22 15:23:16 -0600

    adds admin/default user separation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,17 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :require_admin
 
   def index
-    
+  end
+
+  private
+
+  def require_admin
+    render file: "/public/404" unless current_admin?
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
   end
   
 end
--- spec/views/admin_sees_categories_index_spec.rb
+++ spec/views/admin_sees_categories_index_spec.rb
@@ -11,4 +11,18 @@ describe "User visits categories index page" do
       expect(page).to have_content("Admin Categories")
     end
   end
+
+  context "as a default user" do
+    it "doesn't allow default user to see admin categories index page" do
+      user = User.create(username: "Foo", password: "Baz", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_categories_path
+      expect(page).to_not have_content("Admin Categories")
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+    
+  end
+  
 end
